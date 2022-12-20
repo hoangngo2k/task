57c526241b2d1263b8265a18e6884cc1f7864311
hoangngo2k <hoangngo2k0@gmail.com>
2022-12-20 13:34:37 +0700

    update

--- Test1/src/Exercise2/Bai2.java
+++ Test1/src/Exercise2/Bai2.java
@@ -8,13 +8,13 @@ public class Bai2 {
         Scanner input = new Scanner(System.in);
         System.out.print("Nhập mức tiêu thụ điện: ");
         int consumption = input.nextInt();
-        if (consumption >= 0 && consumption < 25)
+        if (consumption >= 0 && consumption < 25) {
             System.out.println("Số tiền điện phải trả là " + consumption*1000 + "đ");
-        else if (consumption >= 25 && consumption < 75)
-            System.out.println("Số tiền điện phải trả là " + (25*1000 + (consumption-25)*1250) + "đ");
-        if (consumption >= 75 && consumption < 150)
-            System.out.println("Số tiền điện phải trả là " + (25*1000 + (consumption-25-75)*1800 + 75*1250) + "đ");
-        if (consumption >= 150)
+        } else if (consumption >= 25 && consumption < 75) {
+            System.out.println("Số tiền điện phải trả là " + (25 * 1000 + (consumption - 25) * 1250) + "đ");
+        } else if (consumption >= 75 && consumption < 150) {
+            System.out.println("Số tiền điện phải trả là " + (25 * 1000 + (consumption - 25 - 75) * 1800 + 75 * 1250) + "đ");
+        } else if (consumption >= 150)
             System.out.println("Số tiền điện phải trả là " + (25*1000 + (consumption-25-75-150)*2500 + 75*1250 + 150*1800) + "đ");
     }
 }
